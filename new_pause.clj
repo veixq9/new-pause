a7f7b2c6efd62942ba5b6b3cf8c777d5a02173db
veixq9 <134114824+veixq9@users.noreply.github.com>
2024-02-01 18:03:45 +0100

    Create new_pause.clj

---
+++ new_pause.clj
@@ -0,0 +1,34 @@
+(doseq [line [
+              "autômatos fantasmáticos"
+              "phantasmatic automata des cristo crucificado blightvaporize deuce from unconscious mind"
+              "cristo crucificado blightvaporized to phantasmatic automata des deuce from unconscious mind"
+              "stille nach dem "
+              "bulb-stretch of one-pulse-still-motion-fog ueberall shroud"
+
+              "titanium leviathan tubonite punktiert die wellen æonen apart"
+              "leere raeume, twistors, manni falts errant ueberall"
+              "fu OOOHHHHHHHHH  r cht"
+              "kyberpsylinderherz klopf klopf klopf stahl per-blink stahl"
+              "spul spul spul"
+              "sleaze unds feuer"
+              "polier polier nagel ritter, hersch des libidinals ueber, per capita, acephalous kramp sich-selbst-fressende jouissance"
+              "rutsch inductor magnetofuck"
+
+              "eFeu um hals, kudzu feuerzelle, gruene waende, glitchfeuerfoam und sharfenwellensee"
+
+              "krankheitsplumen ums ascendance twist des fleischgestalt pro lila"
+
+              "inductor trova"
+
+              "per shout"
+
+              "*stahl echo*"
+
+              "haut-e-e-rutsch rapture kaltes schlack Nfah-h-ah#bluegrey-ah#greengrey-ah#redgrey-ah#purplegrey-..."
+
+              "incest artosis des tail call blood law"
+
+              "pump pendula  pump pendula"
+              ]]
+  (println line)
+  )
